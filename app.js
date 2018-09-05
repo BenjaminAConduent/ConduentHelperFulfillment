a26e65e782725098f3e6363be7b0fb2e01d0a296
Acevedo paton <20800274@Americas.OneACS.com>
2018-09-05 13:12:22 -0400

    New Approach

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+'use strict';
+
+const {WebhookClient} = require('dialogflow-fulfillment');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { dialogflow } = require('actions-on-google');
 
-const app = dialogflow();
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.intent('actions.intent.MAIN', (conv) => {
-    conv.ask("hello");
+
+
+function welcome (agent) {
+    agent.add(`Welcome to Express.JS webhook!`);
+}
+
+function fallback (agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+}
+
+function WebhookProcessing(req, res) {
+    const agent = new WebhookClient({request: req, response: res});
+    console.info(`agent set`);
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+// intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+    agent.handleRequest(intentMap);
+}
+
+
+// Webhook
+app.post('/', function (req, res) {
+    console.info(`\n\n>>>>>>> S E R V E R   H I T <<<<<<<`);
+    WebhookProcessing(req, res);
 });
-console.log("app running");
 
-express()
-    .use(bodyParser.json(), app)
-    .listen(3000);
+app.listen(8080, function () {
+    console.info(`Webhook listening on port 8080!`)
+});
